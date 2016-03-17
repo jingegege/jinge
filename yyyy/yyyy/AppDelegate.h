60cb4502df6d439c34311c84b27108a2fad051fe
lanou3g <18830052050jin@sina.com>
2016-03-17 10:00:04 +0800

    第一次更改

---
+++ yyyy/yyyy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yyyy
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 JinYaFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yyyy/yyyy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  yyyy
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 JinYaFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
